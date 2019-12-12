5556cbd17d0f14a0fb1c85cc3e1840c580764fc4
Jordan Denning <jordan373@gmail.com>
2019-12-12 12:52:19 -0800

    testing Horizontal win

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -66,4 +66,21 @@ ASSERT_EQ(piezasTest.dropPiece(3), O);
 ASSERT_EQ(piezasTest.gameState(), Blank);
 }
 
+TEST(PiezasTest, gameStateTestXHorizontal)
+{
+Piezas piezasTest;
+ASSERT_EQ(piezasTest.dropPiece(0), X);
+ASSERT_EQ(piezasTest.dropPiece(0), O);
+ASSERT_EQ(piezasTest.dropPiece(1), X);
+ASSERT_EQ(piezasTest.dropPiece(0), O);
+ASSERT_EQ(piezasTest.dropPiece(2), X);
+ASSERT_EQ(piezasTest.dropPiece(1), O);
+ASSERT_EQ(piezasTest.dropPiece(3), X);
+ASSERT_EQ(piezasTest.dropPiece(2), O);
+ASSERT_EQ(piezasTest.dropPiece(1), X);
+ASSERT_EQ(piezasTest.dropPiece(2), O);
+ASSERT_EQ(piezasTest.dropPiece(3), X);
+ASSERT_EQ(piezasTest.dropPiece(3), O);
+ASSERT_EQ(piezasTest.gameState(), X);
+}
 
