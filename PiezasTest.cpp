70909305327c4f5f606e4b0c90e321d89e1b94f4
Jordan Denning <jordan373@gmail.com>
2019-12-11 23:57:22 -0800

    more testing

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -18,13 +18,13 @@ TEST(PiezasTest, dropPieceTest)
 {
     Piezas piezasTest;
 	ASSERT_EQ(piezasTest.dropPiece(0), X);
-//    ASSERT_EQ(piezasTest.dropPiece(0), O);
-//    ASSERT_EQ(piezasTest.dropPiece(1), X);
-//    ASSERT_EQ(piezasTest.dropPiece(2), O);
-//    ASSERT_EQ(piezasTest.dropPiece(1), X);
-//    ASSERT_EQ(piezasTest.dropPiece(0), O);
-//    ASSERT_EQ(piezasTest.dropPiece(0), Blank);
-//    ASSERT_EQ(piezasTest.dropPiece(4), Invalid);
+    ASSERT_EQ(piezasTest.dropPiece(0), O);
+    ASSERT_EQ(piezasTest.dropPiece(1), X);
+    ASSERT_EQ(piezasTest.dropPiece(2), O);
+    ASSERT_EQ(piezasTest.dropPiece(1), X);
+    ASSERT_EQ(piezasTest.dropPiece(0), O);
+    ASSERT_EQ(piezasTest.dropPiece(0), Blank);
+    ASSERT_EQ(piezasTest.dropPiece(4), Invalid);
 }
 
 //TEST(PiezasTest, pieceAtTest)
