f80e681f8e6a8d50d17710ff157ab45f640be947
Jordan Denning <jordan373@gmail.com>
2019-12-12 01:03:06 -0800

    more testing

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -55,7 +55,15 @@ ASSERT_EQ(piezasTest.dropPiece(1), O);
 ASSERT_EQ(piezasTest.dropPiece(2), X);
 ASSERT_EQ(piezasTest.dropPiece(3), O);
 ASSERT_EQ(piezasTest.gameState(), Invalid);
-
+ASSERT_EQ(piezasTest.dropPiece(0), X);
+ASSERT_EQ(piezasTest.dropPiece(0), O);
+ASSERT_EQ(piezasTest.dropPiece(1), X);
+ASSERT_EQ(piezasTest.dropPiece(1), O);
+ASSERT_EQ(piezasTest.dropPiece(2), X);
+ASSERT_EQ(piezasTest.dropPiece(2), O);
+ASSERT_EQ(piezasTest.dropPiece(3), X);
+ASSERT_EQ(piezasTest.dropPiece(3), O);
+ASSERT_EQ(piezasTest.gameState(), Blank);
 }
 
 
