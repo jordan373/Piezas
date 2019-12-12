8b51a918ee98b943866e7278460fb04da2f0e4b1
Jordan Denning <jordan373@gmail.com>
2019-12-12 13:27:43 -0800

    testing X vert win

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -130,7 +130,23 @@ piezasTest.reset();
 ASSERT_EQ(piezasTest.gameState(), Invalid);
 }
 
-
+TEST(PiezasTest, gameStateTestXvertical)
+{
+Piezas piezasTest;
+ASSERT_EQ(piezasTest.dropPiece(0), X);
+ASSERT_EQ(piezasTest.dropPiece(1), O);
+ASSERT_EQ(piezasTest.dropPiece(1), X);
+ASSERT_EQ(piezasTest.dropPiece(2), O);
+ASSERT_EQ(piezasTest.dropPiece(3), X);
+ASSERT_EQ(piezasTest.dropPiece(2), O);
+ASSERT_EQ(piezasTest.dropPiece(0), X);
+ASSERT_EQ(piezasTest.dropPiece(3), O);
+ASSERT_EQ(piezasTest.dropPiece(0), X);
+ASSERT_EQ(piezasTest.dropPiece(3), O);
+ASSERT_EQ(piezasTest.dropPiece(2), X);
+ASSERT_EQ(piezasTest.dropPiece(1), O);
+ASSERT_EQ(piezasTest.gameState(), X);
+}
 
 
 
