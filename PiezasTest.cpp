c444d21dcd671b923be956f1a497f1260d5e5f76
Jordan Denning <jordan373@gmail.com>
2019-12-12 13:15:16 -0800

    testing reset match

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -110,6 +110,27 @@ ASSERT_EQ(piezasTest.dropPiece(0), X);
 ASSERT_EQ(piezasTest.gameState(), Invalid);
 }
 
+TEST(PiezasTest, resetGameTest)
+{
+Piezas piezasTest;
+ASSERT_EQ(piezasTest.dropPiece(0), X);
+ASSERT_EQ(piezasTest.dropPiece(0), O);
+ASSERT_EQ(piezasTest.dropPiece(0), X);
+ASSERT_EQ(piezasTest.dropPiece(1), O);
+ASSERT_EQ(piezasTest.dropPiece(2), X);
+ASSERT_EQ(piezasTest.dropPiece(1), O);
+ASSERT_EQ(piezasTest.dropPiece(3), X);
+ASSERT_EQ(piezasTest.dropPiece(3), O);
+ASSERT_EQ(piezasTest.dropPiece(2), X);
+ASSERT_EQ(piezasTest.dropPiece(2), O);
+ASSERT_EQ(piezasTest.dropPiece(3), X);
+ASSERT_EQ(piezasTest.dropPiece(1), O);
+ASSERT_EQ(piezasTest.gameState(), O);
+piezasTest.reset();
+ASSERT_EQ(piezasTest.gameState(), Invalid);
+}
+
+
 
 
 
